1b34e6a407bf8df03340eb18975a9b6c714ffeb0
Maxime Michon <max.michon00@gmail.com>
2023-01-02 12:01:00 +0100

    initial + j1 matin

---
+++ CSharp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+Test C1 = new Test();
+
+C1.booleen = true;
+
+Test C2 = C1;
+//type référence, ici C2 est égal a l'adresse mémoire de la valeur C1 pas a la valeur de C1
+// on peut utiliser les mot-clés réseervé en mettant des @ devant par exemple
+C1.booleen = false;
+
+var test = new Test(); // le compilateur va deviner le type de la variable
+
+//string myObject = obj?.foo?.bar;
+
+//if (obj != null && obj.foo != null)
+//{
+//    myObject = obj.foo.bar;
+//}
+//même syntaxe entre les deux
+int i = 20;
+
+bool b = i >= 20 ? true : false;
+Console.WriteLine(b);
+
+
+short s = 300;
+byte d = (byte)s;
+Console.WriteLine(d);
+
+const int heureParJour = 24;
+
+joursDeLaSemaine Aujoudhui = joursDeLaSemaine.Lundi;
+Console.WriteLine(joursDeLaSemaine);
+
+enum joursDeLaSemaine
+{
+    Lundi,
+    Mardi,
+    Mercredi,
+    Jeudi,
+    Vendredi,
+    Samedi,
+    Dimanche
+}
+
+class Test
+{
+    public bool booleen;
+}
+
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+
+        }
+    }
+}
+
+
